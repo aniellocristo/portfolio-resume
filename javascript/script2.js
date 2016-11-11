744678d9a217c5e626f8f7f168e65cd23e7230a3
Aniello Cristo <aniello.cristo@gmail.com>
2016-11-11 13:05:17 -0600

    Refined Projects Section

--- javascript/script2.js
+++ javascript/script2.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
 	$(".work-1").mouseover(function() {
-		$(this).children(".hover-1").show();
+		$(this).children(".hover-1").fadeIn().show();
   });
 
   $(".work-1").mouseleave(function(){
-    $(this).children(".hover-1").hide();
+    $(this).children(".hover-1").fadeOut();
 	});
-  
+
 });
